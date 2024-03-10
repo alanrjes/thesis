0e7603f17be8c36d944853a1dadb853f95aaf076
Alan <alanrjes@gmail.com>
2024-03-09 17:29:38 -0800

    debugged classic implementation

---
+++ classic_config/config.py
@@ -0,0 +1,25 @@
+# located in gem5/configs/IBLP
+
+import argparse
+from system import StreamlinedSystem as System
+
+# execution parameters
+parser = argparse.ArgumentParser(description="Config for a simple model of an item-block layered cache.")
+parser.add_argument("binary",
+                    nargs="?",
+                    type=str,
+                    help="Path to the binary to execute.",
+                    default="tests/test-progs/hello/bin/x86/linux/hello")
+parser.add_argument("--cache_type", "-c",
+                    nargs="?",
+                    help="Which cache model to test",
+                    type=str,
+                    choices=["Item", "Block", "IBLP"],
+                    default="Item")
+
+args = parser.parse_args()
+
+system = System()
+system.setup_caches(args.cache_type)
+system.setup_memory()
+system.run(args.binary)
---
+++ classic_wrapper.py
@@ -0,0 +1,71 @@
+import sys
+import os
+import argparse
+import shutil
+import linecache
+import subprocess
+
+# args for running tests
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("binary",
+                    nargs="?",
+                    type=str,
+                    help="Path to the binary to execute.",
+                    default="tests/test-progs/hello/bin/x86/linux/hello")
+parser.add_argument("--trials", "-t",
+                    type=int,
+                    help="Number of trials to run per cache type.",
+                    default=1)
+
+options = parser.parse_args()
+
+here = os.getcwd()
+local_config = "/classic_config"
+run_file = "/config.py"
+
+# copy files to draft and config subdirectories
+def push_files():
+    # check gem5 configs subdirectory for appropriate folder
+    if not os.path.isdir(here + "/gem5/configs" + local_config):
+        os.mkdir(here + "/gem5/configs" + local_config)
+
+    files = os.listdir(here + local_config)
+    for fname in files:
+        if fname=="README":
+            continue
+        # copy to Gem5 configs subdirectory
+        shutil.copy(here + local_config + "/" + fname, here + "/gem5/configs" + local_config)
+        # copy to draft subdirectory
+        shutil.copy(here + local_config + "/" + fname, here + "/draft/code" + local_config)
+
+# run gem5 config for some particular cache model
+def run_config(binary, cacheModel):
+    cmd = "build/X86/gem5.opt configs/classic_config/config.py "+binary
+    out = subprocess.check_output(cmd, shell=True, cwd="./gem5")
+    return out.decode('utf-8').split("\n")[-1]
+
+push_files()
+
+# execution & analysis
+options = parser.parse_args()
+trials = int(options.trials)
+results = {"Item": {"Miss rate":0},
+           "Block": {"Miss rate":0},
+           "IBLP": {"Item layer miss rate":0, "Block layer miss rate":0}}
+
+for cache in ["Item", "Block", "IBLP"]:
+    for i in range(trials):
+        run_config(options.binary, cache)
+        # check stats.txt result
+        f = open("/home/alan/Documents/thesis/gem5/m5out/stats.txt")
+        lines = f.readlines()
+        if cache == "IBLP":
+            results[cache]["Item layer miss rate"] += float(lines[411].split()[1])   # line 412, system.l2cache.overallMissRate::total
+            results[cache]["Block layer miss rate"] += float(lines[519].split()[1])  # line 520, system.l2cache.blockLayer.overallMissRate::total
+        else:
+            results[cache]["Miss rate"] += float(lines[411].split()[1])   # line 412, system.l2cache.overallMissRate::total
+    results[cache] = {k : results[cache][k]/trials for k in results[cache].keys()}
+
+print()
+for k in results:
+    print(k, ":", results[k])
